cb9f6a0e5c29c2ffba2e7b75a607e18b809989f8
rmautia <raphaelnyangenya@gmail.com>
2020-04-20 15:47:25 +0300

    add test for username, credential name and password.

--- credentials_test.py
+++ credentials_test.py
@@ -21,3 +21,18 @@ class TestCredentials(unittest.TestCase):
         """
         # creating the credential object
         self.new_credentials = credentials("pete","gmail","gmail08")
+
+    def tearDown(self):
+        """
+        the tearDown method will help to clean up after every test case is run 
+        """
+        credentials.credentials_list = []
+
+    def test_init(self):
+        """
+        testcase: testing to see whether object is well initialised
+        """
+        self.assertEqual( self.new_credentials.user_password, "pete")
+        self.assertEqual( self.new_credentials.credentials_name, "gmail")
+        self.assertEqual( self.new_credentials.credentials.credentials_password, "gmail08")
+        
